7b130f0f20175dc4d27ffe721d260f371eadad4d
you@example.com <s-rana.dief@zewailcity.edu.eg>
2026-05-12 18:56:55 +0300

    feat(ui): Add difficulty selector  screen and link it to gameplay

---
+++ Assets/_project/Scripts/UI/DifficultyButtonAction.cs
@@ -0,0 +1,41 @@
+using EclipseProtocol.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace EclipseProtocol.UI
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Button))]
+    public class DifficultyButtonAction : MonoBehaviour
+    {
+        [SerializeField] private DifficultyMode difficulty = DifficultyMode.Medium;
+        [SerializeField] private string gameplaySceneName = "Gameplay";
+
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(SelectDifficulty);
+        }
+
+        private void OnDestroy()
+        {
+            if (_button != null)
+            {
+                _button.onClick.RemoveListener(SelectDifficulty);
+            }
+        }
+
+        private void SelectDifficulty()
+        {
+            RunDifficultyData.SetDifficulty(difficulty);
+
+            if (!string.IsNullOrWhiteSpace(gameplaySceneName))
+            {
+                SceneManager.LoadScene(gameplaySceneName);
+            }
+        }
+    }
+}
